8f46d5d802a50f8741a518a6a6a85d9a86e00be4
rajanshrestha <shrestr1@gmatc.matc.edu>
2020-09-16 20:15:43 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+// Connection listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
