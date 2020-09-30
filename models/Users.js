6b02a2de343fb7d1b473fb2683f61f342be6059a
rajanshrestha <shrestr1@gmatc.matc.edu>
2020-09-29 20:19:51 -0500

    Completed Activity 3

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+import e from 'express';
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('user', UserSchema);
+
+export default User;
